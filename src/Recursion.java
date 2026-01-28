29b70e274058add37da6b70ab95f21b511785bd0
hariharan <hariharan9572@gmail.com>
2026-01-29 03:27:05 +0530

    Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,18 @@
+public class Recursion {
+
+    public static void main (String[] args){
+
+       int result = fact(5);
+        System.out.println(result);
+
+    }
+
+    public static int fact(int i){
+
+        if(i!=0)
+            return i * fact(i-1);
+
+        return 1;
+    }
+
+}
