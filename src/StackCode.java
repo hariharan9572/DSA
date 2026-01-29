cce5276a9dff039c9a245c2b4fd97565a493bb73
hariharan <hariharan9572@gmail.com>
2026-01-29 05:49:38 +0530

    Stack Code Pop Peek

---
+++ src/StackCode.java
@@ -0,0 +1,24 @@
+
+
+public class StackCode {
+
+    public static void main(String[] args){
+
+        Stack nums = new Stack();
+
+        nums.pop();
+
+        /*nums.push(10);
+        nums.push(30);
+        System.out.println(nums.pop());
+        nums.push(70);
+        nums.push(20);
+        System.out.println(nums.peek());
+        nums.push(50);
+        nums.push(90);*/
+
+        nums.printStack();
+
+    }
+
+}
